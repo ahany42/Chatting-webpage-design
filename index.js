4aa24d776ae06887ad1c724084bf440c9c3c0c09
Ahany42 <ahanyfathy42@gmail.com>
2023-11-25 08:37:49 +0200

    finalizing webpage

---
+++ index.js
@@ -0,0 +1,11 @@
+
+var mic = document.getElementById("mic");
+var attachment = document.getElementById("attachment");
+var send = document.getElementById("send");
+function fn(){
+  alert("This webpage is a design only");
+}
+
+send.addEventListener('click',fn);
+mic.addEventListener('click',fn);
+attachment.addEventListener('click',fn);
